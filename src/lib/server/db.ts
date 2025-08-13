63773b87c60ea4b9078ae355d269855bb2640e2b
tuan <kullnobita69@gmail.com>
2025-08-13 22:33:28 +1000

    fix process.env in cloud run

--- src/lib/server/db.ts
+++ src/lib/server/db.ts
@@ -1,7 +1,7 @@
 import mysql from "mysql2/promise";
 
 export const db = mysql.createPool({    
-  host: process.env.DB_HOST!,
+  socketPath: `/cloudsql/${process.env.INSTANCE_CONNECTION_NAME}`,
   user: process.env.DB_USER!,
   password: process.env.DB_PASS!,
   database: process.env.DB_NAME!,
