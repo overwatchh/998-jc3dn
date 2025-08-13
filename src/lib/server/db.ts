5d521f6317a4bd6aed331fe980ccf70eb6d2646f
tuan <kullnobita69@gmail.com>
2025-08-14 00:14:00 +1000

    fix process.env in cloud run

--- src/lib/server/db.ts
+++ src/lib/server/db.ts
@@ -1,8 +1,19 @@
 import mysql from "mysql2/promise";
 
-export const db = mysql.createPool({    
-  socketPath: `/cloudsql/${process.env.INSTANCE_CONNECTION_NAME}`,
-  user: process.env.DB_USER!,
-  password: process.env.DB_PASS!,
-  database: process.env.DB_NAME!,
-});
+const isProd = process.env.NODE_ENV === "production";
+
+export const db = mysql.createPool(
+  isProd
+    ? {
+        socketPath: `/cloudsql/${process.env.GCP_PROJECT_ID}:${process.env.GCP_REGION}:${process.env.DB_INSTANCE}`,
+        user: process.env.DB_USER!,
+        password: process.env.DB_PASS!,
+        database: process.env.DB_NAME!,
+      }
+    : {
+        host: process.env.DB_HOST!,
+        user: process.env.DB_USER!,
+        password: process.env.DB_PASS!,
+        database: process.env.DB_NAME!,
+      }
+);
