c1fa50f5efeeba26524b227946e138e0ba635e75
tuan <kullnobita69@gmail.com>
2025-08-13 21:06:12 +1000

    hard code db info

--- src/lib/server/db.ts
+++ src/lib/server/db.ts
@@ -1,8 +1,8 @@
 import mysql from "mysql2/promise";
 
 export const db = mysql.createPool({  
+  host: '35.197.171.70',
   user: 'root',
   password: 'JC3DNstrong@Password',
-  database: 'qr_attendance_app',
-  socketPath: '/cloudsql/project-capstone-468109:australia-southeast1:jc3dn-998-db'
+  database: 'qr_attendance_app',  
 });
