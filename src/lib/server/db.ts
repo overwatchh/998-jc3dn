5f998fcaca343b215cfd5b931f9bf557afdb00f2
tuan <kullnobita69@gmail.com>
2025-08-13 19:51:19 +1000

    hard code db info

--- src/lib/server/db.ts
+++ src/lib/server/db.ts
@@ -1,7 +1,6 @@
 import mysql from "mysql2/promise";
 
-export const db = mysql.createPool({
-  host: '35.197.171.70',
+export const db = mysql.createPool({  
   user: 'root',
   password: 'JC3DNstrong@Password',
   database: 'qr_attendance_app',
