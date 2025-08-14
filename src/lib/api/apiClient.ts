1a8f81ec999ee3131d90741a6c6ef28e472cd29b
ddp-vn <ddp29596@gmail.com>
2025-08-14 17:55:15 +0700

    fix base url

--- src/lib/api/apiClient.ts
+++ src/lib/api/apiClient.ts
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const apiClient = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_URL,
+  baseURL: "/api",
   withCredentials: true, // Important for sending/receiving cookies
   headers: {
     "Content-Type": "application/json",
