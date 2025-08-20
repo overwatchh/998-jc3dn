be8ec6b6a92336fa0d7ec9e0b7b6c2693d74695d
Justin Quinn <39190634+snowmanmedia@users.noreply.github.com>
2025-08-20 13:57:06 +1000

    Update db_drop.sql
    changed order for dropping to better reflect

--- src/lib/server/db_schema/db_drop.sql
+++ src/lib/server/db_schema/db_drop.sql
@@ -4,12 +4,12 @@ USE qr_attendance_app;
 -- Drop child tables first (due to foreign key dependencies)
 DROP TABLE IF EXISTS attendance;
 DROP TABLE IF EXISTS event;
+DROP TABLE IF EXISTS subject_enrolment;
 DROP TABLE IF EXISTS class;
 DROP TABLE IF EXISTS room;
 DROP TABLE IF EXISTS campus;
 DROP TABLE IF EXISTS session_enrolment;
 DROP TABLE IF EXISTS study_session;
-DROP TABLE IF EXISTS subject_enrolment;
 DROP TABLE IF EXISTS subject;
 DROP TABLE IF EXISTS semester;
 DROP TABLE IF EXISTS `verification`;
