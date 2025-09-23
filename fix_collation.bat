dcfe7d7ccab40e135197f2f4f464765ae12b4430
sunard79 <60280078+sunard79@users.noreply.github.com>
2025-09-23 15:30:07 +1000

    Delete fix_collation.bat

--- fix_collation.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-echo Fixing SQL collation for XAMPP compatibility...
-powershell -Command "(gc qr_attendance_app_dump.sql) -replace 'utf8mb4_0900_ai_ci', 'utf8mb4_general_ci' | Out-File -encoding ASCII qr_attendance_app_xampp.sql"
-echo Fixed file saved as: qr_attendance_app_xampp.sql
-pause
