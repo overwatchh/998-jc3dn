a620e90ad0287fd737b4e643ff9836804d756726
sunard79 <60280078+sunard79@users.noreply.github.com>
2025-09-23 15:29:41 +1000

    Delete qr_attendance_app_setup.sql

--- qr_attendance_app_setup.sql
+++
@@ -1,8 +0,0 @@
--- MySQL dump file for qr_attendance_app database
--- Run this file in phpMyAdmin or MySQL command line
-
-CREATE DATABASE IF NOT EXISTS qr_attendance_app;
-USE qr_attendance_app;
-
--- Add your table structures and data here
--- Export from MySQL Workbench and paste below
